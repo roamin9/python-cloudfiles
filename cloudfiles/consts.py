4391ebb543a34d6aad38ba4a46877ecffd00877e
roamin9 <zhoo.xuan@gmail.com>
2011-05-16 11:17:19 +0800

    from devauth to swauth Change swauth's default authurl

--- cloudfiles/consts.py
+++ cloudfiles/consts.py
@@ -7,7 +7,10 @@ __version__ = "1.8.0.0"
 user_agent = "python-cloudfiles/%s" % __version__
 # https and no port will support later
 #chouti_authurl = 'https://auth.api.rackspacecloud.com/v1.0'
-chouti_authurl = 'http://chouti.roamin9.me:11000/v1.0'
+# for devauth
+#chouti_authurl = 'http://chouti.roamin9.me:11000/v1.0'
+# for swauth
+chouti_authurl = 'http://chouti.roamin9.me:8080/auth/v1.0'
 default_authurl = chouti_authurl
 
 # swift object name, container name, meta name, meta value have some limits
